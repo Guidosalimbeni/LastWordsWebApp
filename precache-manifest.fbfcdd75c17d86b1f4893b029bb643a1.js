4f93324cb793de535542fe1a68282650eaf83bd7
Guido Salimbeni <guido.salimbeni@gmail.com>
2021-06-17 22:05:08 +0100

    Updates

---
+++ precache-manifest.fbfcdd75c17d86b1f4893b029bb643a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9bf5400da66e520da35",
+    "url": "/LastWordsWebApp/static/css/main.b8ee144d.chunk.css"
+  },
+  {
+    "revision": "f9bf5400da66e520da35",
+    "url": "/LastWordsWebApp/static/js/main.f9bf5400.chunk.js"
+  },
+  {
+    "revision": "074a93170ece633888dc",
+    "url": "/LastWordsWebApp/static/js/1.074a9317.chunk.js"
+  },
+  {
+    "revision": "30dfd95af27adedb5200",
+    "url": "/LastWordsWebApp/static/js/runtime~main.30dfd95a.js"
+  },
+  {
+    "revision": "9d2c3d896c59e3b7fa0fbd95d12e696a",
+    "url": "/LastWordsWebApp/index.html"
+  }
+];
